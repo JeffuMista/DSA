d06f383dbf597a0c4c5e70008a363981a47b122e
JeffuMista <kariukigeoffrey18@gmail.com>
2025-07-28 15:07:44 +0300

    initial Repo

---
+++ Arrays.py
@@ -0,0 +1,15 @@
+arr = [10,20,20, 40, 50, 60] #Declare an array
+
+arr[2]=30 #Update an element in the array
+
+arr.append(70) # Append an element to the array
+
+arr.insert(1,15) # Insert an element at a specific index
+
+arr.pop(3) # Remove an element at a specific index
+
+print(arr) # Output the modified array
+
+if 70 in arr:
+    print("70 is in the array") # Check if an element exists in the array
+
